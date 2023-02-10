57aa73cb96e557ae60cb21107b64cec16179d655
trodrigu <trodriguez91@icloud.com>
2023-02-10 06:49:15 -0700

    wire in reducer

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,15 @@
+use mathy_core::parse::{
+    total_action,
+    Action::{DefineFunc, DefineVar, EvalExpr},
+    Token,
+};
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::to_value;
+use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
-use std::collections::HashMap;
-use mathy_core::parse::Token;
 
 #[wasm_bindgen]
 extern "C" {
@@ -22,79 +27,109 @@ struct EquationArgs<'a> {
 
 #[derive(Clone, PartialEq, Debug)]
 struct State {
-    current_equation: Option<String>,
+    current_equation_or_action: Option<String>,
     tokens: HashMap<String, Token>,
-    last_evaluated: Option<String>
+    last_evaluated: Option<String>,
 }
 
-impl State {
-    pub fn default() -> State {
+impl Default for State {
+    fn default() -> Self {
         Self {
-            current_equation: Some("".to_string()),
+            current_equation_or_action: None,
             tokens: HashMap::new(),
-            last_evaluated: Some("".to_string())
+            last_evaluated: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum StateAction {
+    EvalOrDefine(String),
+}
+
+impl Reducible for State {
+    type Action = StateAction;
+
+    fn reduce(self: std::rc::Rc<Self>, action: Self::Action) -> std::rc::Rc<Self> {
+        log(&"yo!");
+        match action {
+            StateAction::EvalOrDefine(equation_or_action) => {
+                let mut current_tokens = self.tokens.clone();
+                let action = total_action().parse(equation_or_action.as_bytes()).unwrap();
+
+                match action {
+                    DefineFunc(func_name, args, expr) => {
+                        current_tokens.insert(func_name, expr);
+                        Self {
+                            current_equation_or_action: None,
+                            tokens: current_tokens,
+                            last_evaluated: Some(equation_or_action),
+                        }
+                        .into()
+                    }
+                    DefineVar(var_name, var) => {
+                        current_tokens.insert(var_name, var);
+                        Self {
+                            current_equation_or_action: None,
+                            tokens: current_tokens,
+                            last_evaluated: Some(equation_or_action),
+                        }
+                        .into()
+                    }
+                    EvalExpr(expr) => {
+                        let tokens = current_tokens.clone();
+                        let last_evaluated =
+                            Some(expr.eval(current_tokens.clone()).unwrap().to_string());
+                        Self {
+                            current_equation_or_action: None,
+                            tokens: tokens,
+                            last_evaluated: last_evaluated,
+                        }
+                        .into()
+                    }
+                }
+            }
         }
     }
 }
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let equation_input_ref = use_node_ref();
-
-    let equation = use_state(|| State::default().current_equation.unwrap());
-
-    let equation_msg = use_state(|| State::default().last_evaluated.unwrap());
-    {
-        let equation_msg = equation_msg.clone();
-        let equation = equation.clone();
-        let updated_equation = equation.clone();
-        use_effect_with_deps(
-            move |_| {
-                spawn_local(async move {
-                    if equation.is_empty() {
-                        return;
-                    }
-
-                    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-                    let new_msg = invoke(
-                        "evaluate_equation",
-                        to_value(&EquationArgs {
-                            equation: &*equation,
-                        })
-                        .unwrap(),
-                    )
-                    .await;
-                    log(&new_msg.as_string().unwrap());
-                    equation_msg.set(new_msg.as_string().unwrap());
-                });
-
-                || {}
-            },
-            updated_equation,
-        );
-    }
+    let state = use_reducer(|| State::default());
 
     let evaluate_equation = {
-        let equation = equation.clone();
-        let equation_input_ref = equation_input_ref.clone();
-        Callback::from(move |_| {
-            equation.set(
-                equation_input_ref
-                    .cast::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            );
+        let state = state.clone();
+        Callback::from(move |equation_or_action| {
+            state.dispatch(StateAction::EvalOrDefine(equation_or_action))
         })
+    };
+
+    let onkeypress = {
+        move |e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let input: HtmlInputElement = e.target_unchecked_into();
+                let value = input.value();
+
+                input.set_value("");
+                evaluate_equation.emit(value);
+            }
+        }
     };
 
     html! {
         <main class="container">
             <div class="row">
-                <input id="equation-input" ref={equation_input_ref} placeholder="Enter an equation..." />
-                <button type="button" onclick={evaluate_equation}>{"Evaluate"}</button>
+                <input id="equation-input" {onkeypress} placeholder="Enter an equation..." />
             </div>
 
-            <p><b>{ &*equation_msg }</b></p>
+            <p><b>
+            {
+                if let Some(evaluated) = &state.last_evaluated {
+                    evaluated.clone()
+                } else {
+                    "None Evaluated Yet!".to_string()
+                }
+            }</b></p>
         </main>
     }
 }
